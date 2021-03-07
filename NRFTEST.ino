b2f94441c6e44c6390838c56d20722e7238f825a
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2021-03-06 20:33:37 -0800

    Update NRFTEST.ino
    Updated code flexibility for device design and device wireless specifications

--- NRFTEST.ino
+++ NRFTEST.ino
@@ -1,32 +1,33 @@
+/*Copyright Berkan Mertan 2021*/
 //nrf2401 transmitter:
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#define PINBASE 30
+//Buzzer/speaker pin and transmission button pin. Can be adjusted for different designs.
 #define BUZZERPIN 10
-RF24 nrf(9, 8);  // CE, CSN
-const byte linkAddress[6] = "link1";
-
+#define BUTTONPIN 7
+RF24 nrf(9, 8);  // CE, CSN pins for transmitter. Can be adjusted for different designs.
+const byte linkAddress[6] = "link1"; //linkAddress buffer for specifying the pairing channel
+//Sample data value holders and buffers
 float val1;   
 float val2;  
 float val3; 
 float data[3];
 char charVal[17];
 ////////////////////////////////////////////////////
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Serial.println("BMP280/NRF24L01 link");
   nrf.begin();   
   nrf.openWritingPipe(linkAddress);  //set the address 
   //nrf.setPALevel(RF24_PA_LOW);   //keep tx level low to stop psu noise, can remove this but then put in decoupling caps
   // Options are: RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm,  RF24_PA_HIGH=0dBm.
-  nrf.setPALevel(RF24_PA_LOW); 
+  nrf.setPALevel(RF24_PA_LOW);  //-12dB, optimal for lower power consumption. To increase range, 0dBm or RF24_PA_HIGH recommended
   nrf.stopListening();  //act as a transmitter
 }
 ///////////////////////////////////////////////////
-void loop()
-{  
+void loop() {
+ //Sample data buffer values, here to be changed to whatever data you want to transmit
  val1 = 1;
  val2 = 2;
  val3 = 3;
@@ -34,18 +35,19 @@ void loop()
  data[0] = val1;
  data[1] = val2;
  data[2] = val3;
- if(digitalRead(7) == HIGH) {
-    Serial.println("HIGH!");
-    tone(BUZZERPIN, 262, 250);
+ //Check for current reading on pin 7
+ if(digitalRead(BUTTONPIN) == HIGH) {
+    Serial.println("HIGH!"); //For debugging
+    tone(BUZZERPIN, 262, 250); //Just sample frequency and tone length
     nrf.write(data, sizeof(data)); //spit out the data 
-    delay(250*1.3);
-    noTone(BUZZERPIN);
+    delay(250*1.3); //For tone
+    noTone(BUZZERPIN); //For tone
  }
  //----- display on local monitor: ------------
  Serial.print(data[0]); Serial.print("  ");
  Serial.print(data[1]); Serial.print("  ");
  Serial.print(data[2]); Serial.print("  ");
  //---------------------------------------------
- delay(300);
+ delay(300); //Recommended delay time for checking pushbutton current state
 }
 /////////////////////////////////////////////////
